9672049475168053990b6ecf8215b58b75ad2709
Amrutaraj31 <161153471+Amrutaraj31@users.noreply.github.com>
2026-03-05 15:08:31 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,12 +1,23 @@
-
-
 document.addEventListener("DOMContentLoaded", function () {
 
     const menuToggle = document.getElementById("menuToggle");
     const sidebar = document.getElementById("sidebar");
+    const closeBtn = document.getElementById("closeSidebar");
 
     menuToggle.addEventListener("click", function () {
         sidebar.classList.toggle("active");
+    });
+
+    closeBtn.addEventListener("click", function () {
+        sidebar.classList.remove("active");
+    });
+
+    const links = sidebar.querySelectorAll("a");
+
+    links.forEach(link => {
+        link.addEventListener("click", function () {
+            sidebar.classList.remove("active");
+        });
     });
 
 });
