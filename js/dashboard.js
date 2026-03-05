b24b34662ab1905eee74a14dad5b1f00a8a2f00b
Amrutaraj31 <161153471+Amrutaraj31@users.noreply.github.com>
2026-03-05 15:35:06 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,15 +1,19 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function(){
 
-    const menuToggle = document.getElementById("menuToggle");
-    const sidebar = document.getElementById("sidebar");
-    const backBtn = document.getElementById("backBtn");
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
+const backBtn = document.getElementById("backBtn");
 
-    menuToggle.addEventListener("click", function () {
-        sidebar.classList.add("active");
-    });
+if(menuToggle){
+menuToggle.addEventListener("click",function(){
+sidebar.classList.add("active");
+});
+}
 
-    backBtn.addEventListener("click", function () {
-        sidebar.classList.remove("active");
-    });
+if(backBtn){
+backBtn.addEventListener("click",function(){
+sidebar.classList.remove("active");
+});
+}
 
 });
