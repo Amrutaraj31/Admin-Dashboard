179bbebc5de02b56db17d3ef77f857194a1c89e2
Amrutaraj31 <161153471+Amrutaraj31@users.noreply.github.com>
2026-03-09 13:17:16 +0530

    Update loadSidebar.js

--- js/loadSidebar.js
+++ js/loadSidebar.js
@@ -6,12 +6,6 @@ fetch("sidebar.html")
 
 document.getElementById("sidebar-container").innerHTML = data;
 
-const sidebar = document.getElementById("sidebar");
-
-if(sidebar){
-new bootstrap.Offcanvas(sidebar);
-}
-
 })
 .catch(error => {
 console.error("Sidebar failed to load:", error);
