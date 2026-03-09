617f5e8c9e13ddf8c11c6a60df90c188f2b521b2
Amrutaraj31 <161153471+Amrutaraj31@users.noreply.github.com>
2026-03-09 16:06:37 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,19 +1,1 @@
-document.addEventListener("DOMContentLoaded", function(){
 
-const menuToggle = document.getElementById("menuToggle");
-const sidebar = document.getElementById("sidebar");
-const backBtn = document.getElementById("backBtn");
-
-if(menuToggle){
-menuToggle.addEventListener("click",function(){
-sidebar.classList.add("active");
-});
-}
-
-if(backBtn){
-backBtn.addEventListener("click",function(){
-sidebar.classList.remove("active");
-});
-}
-
-});
